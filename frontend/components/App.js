6a8db2b55d61f174fbb4000a21d7b9f6a608b402
Chris Fritsen <chrisfritsen@gmail.com>
2024-01-21 11:53:50 -0800

    Added State to track Todo List and form.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,13 +3,36 @@ import TodoList from './TodoList'
 import Form from './Form'
 
 export default class App extends React.Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      todo: [],
+      formData: ''
+    }
+  }
+
+  formValue = evt => {
+    this.setState({...this.state, formData: evt.target.value})
+  }
+
+  addTask = evt => {
+    evt.preventDefault();
+    this.setState({
+      todo: [...this.state.todo, {
+        name: this.state.formData,
+        id: new Date(),
+        completed: false
+      }],
+      formData: ''
+    })
+  }
+
   render() {
     return (
       <div>
         Todo App
-        <TodoList />
-        <Form />
+        <TodoList taskList={this.state.todo} />
+        <Form addTask={this.addTask} formValue={this.formValue} />
       </div>
     )
   }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Todo from './Todo'
 
 export default class TodoList extends React.Component {
+  //this.props.taskList
   render() {
     return (
       <div>
