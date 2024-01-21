3399f243b86e1e384d882890d07f7f77fadfa621
Chris Fritsen <chrisfritsen@gmail.com>
2024-01-21 10:49:27 -0800

    Basic component structure

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 
 export default class App extends React.Component {
+  
   render() {
     return (
       <div>
         Todo App
+        <TodoList />
+        <Form />
       </div>
     )
   }
